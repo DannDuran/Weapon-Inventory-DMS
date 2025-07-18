927428e3397319ce2da7745870e82eac8d1e3713
DannDuran <134342373+DannDuran@users.noreply.github.com>
2025-07-18 17:01:52 -0400

    Add files via upload

---
+++ javadoc/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.javafx_project","l":"Main"},{"p":"com.example.javafx_project","l":"weapon"}];updateSearchResults();
